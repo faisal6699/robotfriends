82104196004e2708d036a6cb929c43cca8e1e1ee
faisal6699 <faisal.ahmed6699@gmail.com>
2020-06-15 15:38:48 +0600

    card changed

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Card = ({ id,name,email}) => {
+	const Url = 'https://robohash.org/'
 
 	return (
 		<div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
-			<img alt='robots' src={`https://robohash.org/${id}` } className='w-50'/>
+			<img alt='robots' src={`${Url}${id}` } className='w-50'/>
 			<div>
 				<h5>{name}</h5>
 				<p>{email}</p>
